f76f5666b8214ad9a4693197f1fed478f082dfbc
Janardon <janardonknwr09@gmail.com>
2026-08-28 09:56:44 +0530

    Create prblem-08.c

---
+++ C program/prblem-08.c
@@ -0,0 +1,17 @@
+//WAp to find the first and last digit of a number
+#include<stdio.h>
+int main()
+{
+    int num, last, first, number;
+    printf("Enter the number\n");
+    scanf("%d", &num);
+    number = num;
+    last = num % 10;
+    first = num;
+    while(first >= 10)
+    {
+        first = first / 10;
+    }
+    printf("First and last digit of %d is %d and %d\n", number, first, last);
+    return 0;
+}
